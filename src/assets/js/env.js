9637f9ca3b3def710f6823a1163ecf9ea680eda8
Pedro Henrique Krohling Almeida <pdrkrohl@gmail.com>
2024-12-05 21:39:49 -0300

    feat: removido /posts da API_URL

--- src/assets/js/env.js
+++ src/assets/js/env.js
@@ -1,1 +1,1 @@
-export const API_URL = 'https://imersao-instabytes-222262699991.southamerica-east1.run.app/posts';
+export const API_URL = 'https://imersao-instabytes-222262699991.southamerica-east1.run.app';
